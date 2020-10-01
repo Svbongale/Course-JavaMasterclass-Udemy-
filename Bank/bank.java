18742cbf28873e79ddfd42df373a9aacc3dd4324
Savya Sachi V B <savyanio@gmail.com>
2020-10-01 20:31:20 +0530

    Constructors - default and parameterized

---
+++ Bank/bank.java
@@ -0,0 +1,84 @@
+public class bank {
+    private int accNo;
+    private double balance;
+    private String custName;;
+    private String email;
+    private int phNo;
+    
+    // Constructor
+    public bank(){
+        System.out.println("Default constructor called!");
+    }
+
+    // Parameterized constructor
+    public bank(String custName,String email,int phNo,int accNo,double balance){
+        System.out.println("Parametrized constructor called");
+        this.custName = custName;
+        this.email = email;
+        this.accNo = accNo;
+        this.balance = balance;
+        this.phNo = phNo;
+    }
+
+
+
+
+    public int getAccNo() {
+        return this.accNo;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public String getCustName() {
+        return this.custName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getPhNo(){
+        return this.phNo;
+    }
+
+    public void setAccNo(int accNo) {
+        this.accNo = accNo;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhNo(int phNo) {
+        this.phNo = phNo;
+    }
+
+    public void deposit(double amount) {
+        this.balance = this.balance + amount;
+        System.out.println("New balance: " + this.balance);
+    }
+
+    public void withdraw(double amount) {
+        if((this.balance>0.0)&&((this.balance- amount)>0.0))
+        {
+            this.balance = this.balance - amount;
+            System.out.println("Remaining balance: " + this.balance);
+        }        
+        else 
+        System.out.println("Insufficient Balance");
+    }
+
+    public void getDetails() {
+        System.out.println("Name: " + this.custName + "AccNo: " + this.accNo + "Balance:" + this.balance + "Email: " + this.email + "PhNo: " + this.phNo);
+    }
+}
---
+++ Bank/bankMain.java
@@ -0,0 +1,21 @@
+public class bankMain {
+    public static void main(String[] args){
+        bank person = new bank();
+        // without constructor
+        person.withdraw(100.00);
+        person.deposit(1000.00);
+        person.deposit(76.99);
+        person.withdraw(456.88);
+        person.setAccNo(123456789);
+        person.setCustName("SavyaSachi Bongale");
+        person.setEmail("SavyaSachiBongale@gmail.com");
+        person.setPhNo(998855447);
+        person.setBalance(4455059.88);
+        person.withdraw(19888.99);
+        person.getDetails();
+
+        // With constructor
+        bank person1 = new bank("Joe ","joe@gmail.com ",8833477,4433550,88990.00);
+        person1.getDetails();
+    }
+}
